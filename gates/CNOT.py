f70e1934d0107c8a32e9b08ff43d865da21a4ae9
Hao Zhang <howezhang88@gmail.com>
2025-10-18 16:15:39 +0800

    Update CNOT.py
    Add guard code to protect users from accidentally invoking code for testing.

--- gates/CNOT.py
+++ gates/CNOT.py
@@ -26,7 +26,8 @@ class CnotGate:
         return np.dot(self.matrix, state_vector)
 
 
-cnot_gate = CnotGate()
-sample = np.array([0, 0, 1, 0], dtype=complex) # input: |10⟩
+if __name__ == "__main__":
+    cnot_gate = CnotGate()
+    sample = np.array([0, 0, 1, 0], dtype=complex) # input: |10⟩
+    print(cnot_gate.apply(sample))  # output: |11⟩
 
-print(cnot_gate.apply(sample))  # output: |11⟩
