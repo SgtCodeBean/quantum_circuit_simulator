c3fd529aea0d21d7d9149baa2b4425d6267b6262
Hao Zhang <howezhang88@gmail.com>
2025-10-18 16:17:22 +0800

    Update PhaseGate.py
    Add guard code to protect users from accidentally invoking code for testing.

--- gates/PhaseGate.py
+++ gates/PhaseGate.py
@@ -15,6 +15,7 @@ class PhaseGate:
         return np.dot(self.matrix, state_vector)
 
 
-phase_gate = PhaseGate()
-sample = np.array([0, 1], dtype=complex) # input: |1⟩
-print(phase_gate.apply(sample))  # output: j|1⟩
+if __name__ == "__main__":
+    phase_gate = PhaseGate()
+    sample = np.array([0, 1], dtype=complex) # input: |1⟩
+    print(phase_gate.apply(sample))  # output: j|1⟩
